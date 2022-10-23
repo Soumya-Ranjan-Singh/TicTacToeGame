37afab8e82a1cfc8767aba127c328fd344b62e35
Soumya-Ranjan-Singh <soumyars675@gmail.com>
2022-10-24 05:25:31 +0530

    Added : Display Welcome to Tic Tac Toe Game

---
+++ src/com/tictactoe/program/TicTacToe.java
@@ -0,0 +1,19 @@
+//This program is all about Tic Tac Toe Game.
+//Starting is to show a Welcome message to this game.
+
+package com.tictactoe.program;
+
+public class TicTacToe {
+
+    //Creating a default constructor
+    public TicTacToe() {
+        System.out.println("Welcome to Tic Tac Toe Game !!!");
+    }
+
+    public static void main(String[] args) {
+
+        //Initialize the object
+        TicTacToe obj = new TicTacToe();
+
+    }
+}
